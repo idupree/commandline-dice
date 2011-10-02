d398b9467e48c2e4f80f1cbf56f230191627a963
Isaac Dupree <antispam@idupree.com>
2011-10-02 12:55:03 -0400

    Add some documentation/specification
    Some of it seems to be broken right now, but
    the first step was saying what it was supposed to do.
    Then I can write tests/fixes better.
    
    Anyway it needed a good help string; I always forgot
    what my own program's syntax was on the occasions I wanted
    to use it.

--- d.cpp
+++ d.cpp
@@ -28,8 +28,35 @@ void pr(void* k, void* v, void* ud) {
 	printf(":%i %i\n", *(int*)k, *(int*)v);
 }
 
+const char help_string[] =
+"Usage: d sides [times[!]]\n"
+"\t`d` rolls imaginary dice.\n"
+"\n"
+"\t`d 6` prints a number from 1 to 6 inclusive.\n"
+"\t`d 6 10` prints ten numbers from 1 to 6 inclusive.\n"
+"\t`d 6 4!` prints four non-repeating numbers from 1\n"
+"\t\t\tto 6 inclusive.  For example,\n"
+"\t\t\t`d 5 5!`\n"
+"\t\t\tlets you choose a random order\n"
+"\t\t\tfor five items you've lined up.\n"
+"\n"
+"\t\"sides\" may be any number from 1 on up.\n"
+"\t\"times\" can't be negative, and if you want non-repeating.\n"
+"\t\t\tnumbers then must be no greater than sides.\n"
+;
+
 int main(int argc, char** argv)
 {
+	if(argc <= 1) {
+		fprintf(stderr, help_string);
+		exit(1);
+	}
+	if(strcmp(argv[1], "--help") == 0 ||
+	   strcmp(argv[1], "--version") == 0) {
+		fprintf(stdout, help_string);
+		exit(0);
+	}
+
 	rand_init();
 
 	//In case of overflow, atoll returns max (long)long. huh.
